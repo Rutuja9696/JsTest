251da471d2e66309b9d476928156fdac46bd829b
Rutuja9696 <rutuja.patil@contentstact.com>
2020-12-04 12:56:10 +0530

    function to convert time

--- questions/convertTime.js
+++ questions/convertTime.js
@@ -6,6 +6,14 @@
  The value corresponding to each key should be the conversion of seconds into correponding 
  time 
  */
-const convertTime = (seconds) => {};
+const convertTime = (seconds) => {
+  const totalTime = 1;
+  let hours = Math.floor(totalTime / 60 / 60);
+  let minutes = Math.floor(totalTime / 60);
+  let second = totalTime / 60 / 60 / 60;
 
+  return hours + ":" + minutes + ":" + seconds;
+};
+console.log(convertTime(1));
+// convertTime();
 module.exports = convertTime;
